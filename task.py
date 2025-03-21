e696170bcd058e05559ff58b9d9d7fe2bd1d8f7f
Aman kushwaha <amanhtc1112kushwaha@gmail.com>
2025-03-21 21:13:28 +0530

    update task

--- task.py
+++ task.py
@@ -1,48 +1,69 @@
-data={}
-data2={}
+
+
+
+
+data=[]
+student={}
+qulification_list=[]
+qulification={}
 skills=[]
-qulification_information={}
-list_data=[]
 
-data['id']=int(input('please enter your id: '))
-data['name']=input('please enter your name: ')
-data['exprinece']=input('please enter your exprience: ')
-print('please enter your skills! ')
-skills.append(input('please enter your first skills: '))
-skills.append(input('please enter your seonds skills: '))
-skills.append(input('please enter your thirds skills: '))
-data['skills']=skills
-print('please enter your qulification information! ')
-qulification_information['qulificationname']=input('please enter your qulification name: ')
-qulification_information['passingyear']=input('please enter your passing year: ')
-list_data.append(qulification_information)
-data['qulification']=list_data
-qulification_information={}
-qulification_information['qulificationname']=input('please enter your qulification name: ')
-qulification_information['passingyear']=input('please enter your passingyear: ')
+student2={}
 
-list_data.append(qulification_information)
-data['qulification']=list_data
-print('please seconds student information input! ')
-data2['id']=int(input('please enter your id:'))
-data2['name']=input('please enter your name: ')
-data2['exprience']=input('please enter your exprience: ')
-data['']=data2
 
-skills=[]
+student['id']=int(input('please enter your id: '))
+student['name']=input('please enter your name: ')
+student['experience']=input('please enter your experience: ')
 print('please enter your skills!')
 skills.append(input('please enter your first skills: '))
-skills.append(input('please enter your seonds skills: '))
+skills.append(input('plesae enter your seconds skills: '))
 skills.append(input('please enter your thirds skills: '))
+student['skills']=skills
 
-data['skill']=skills
+print('please enter your qulification information!')
 
-qulification_information={}
-list_data=[]
-print('please enter your qulification!')
-qulification_information['qulification name']=input('please enter your qulification name: ')
-qulification_information['passingyear']=input('please enter your passing year: ')
-list_data.append(qulification_information)
-data['qulification2']=list_data
+qulification['name']=input('please enter your qulification name: ')
+qulification['passingyear']=int(input('please enter your passing year: '))
+qulification_list.append(qulification)
+
+student['qulification']=qulification_list
+
+qulification={}
+
+qulification['name']=input('please enter your qulification name: ')
+qulification['passingyear']=int(input('please enter your passing year:'))
+
+qulification_list.append(qulification)
+
+student['qulification']=qulification_list
+data.append(student)
+
+
+print('please enter your second student input information! ')
+
+student2['id']=int(input('please enter your id: '))
+student2['name']=input('please enter your name: ')
+student2['experience']=input('please enter your experience: ')
+
+
+print('please enter your skills!')
+skills=[]
+skills.append(input('please enter your first skills: '))
+skills.append(input('plesae enter your seconds skills: '))
+skills.append(input('please enter your thirds skills: '))
+student2['skills']=skills
+
+
+print('please enter your qulification information!')
+qulification_list=[]
+qulification={}
+
+qulification['name']=input('please enter your qulification name: ')
+qulification['passingyear']=int(input('please enter your passing year: '))
+
+qulification_list.append(qulification)
+
+student2['qulification2']=qulification_list
+data.append(student2)
 
 print(data)
