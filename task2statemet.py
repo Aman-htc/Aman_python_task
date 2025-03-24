c87a4f4f81f04b1055150ebc10d56976a2695391
Aman kushwaha <amanhtc1112kushwaha@gmail.com>
2025-03-24 19:42:54 +0530

    update task statement

--- task2statemet.py
+++ task2statemet.py
@@ -1,13 +1,68 @@
+# list_data=[1,2,3,4,5,6,7,8,9,10]
+
+# if list_data[0]<5:
+    
+#     small_number=[list_data[0],list_data[1],
+
+# list_data[2],list_data[3]]
+
+# if list_data[5]>5:
+#     big_number=[list_data[5],list_data[6],list_data[7],list_data[8],list_data[9] ]  
+#     print(small_number)
+#     print(big_number)
+
+
 list_data=[1,2,3,4,5,6,7,8,9,10]
+small_list=[]
+big_list=[]
 
 if list_data[0]<5:
+    small_list.append(list_data[0])
+else:
+    big_list.append(list_data[0])
+     
+if list_data[1]<5:
+    small_list.append(list_data[0])
+else:
+    big_list.append(list_data[0])
+         
+if list_data[2]<5:
+    small_list.append(list_data[1])
+else:
+    big_list.append(list_data[1])    
+if list_data[2]<5:
+    small_list.append(list_data[2])
+else:
+    big_list.append(list_data[2])    
+if list_data[3]<5:
+    small_list.append(list_data[3])
     
-    small_number=[list_data[0],list_data[1],
-
-list_data[2],list_data[3]]
-
-if list_data[5]>5:
-    big_number=[list_data[5],list_data[6],list_data[7],list_data[8],list_data[9] ]  
-    print(small_number)
-    print(big_number)
-
+else:
+    big_list.append(list_data[3])    
+if list_data[4]<5:
+    small_list.append(list_data[4])
+else:
+    big_list.append(list_data[4])    
+if list_data[5]<5:
+    small_list.append(list_data[5])
+else:
+    big_list.append(list_data[5])    
+if list_data[6]<5:
+    small_list.append(list_data[6])
+else:
+    big_list.append(list_data[6])    
+if list_data[7]<5:
+    small_list.append(list_data[7]) 
+else:
+    big_list.append(list_data[7])     
+if list_data[8]<5:
+    small_list.append(list_data[8])  
+else:
+    big_list.append(list_data[8])    
+if list_data[9]<5:
+    small_list.append(list_data[9])  
+else:
+    big_list.append(list_data[9])                                      
+    
+print(small_list)
+print(big_list)
