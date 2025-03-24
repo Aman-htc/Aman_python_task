6e9b4b0480cfba364ffcf655ba190c8303a898f8
Aman kushwaha <amanhtc1112kushwaha@gmail.com>
2025-03-24 14:09:59 +0530

    task 2

---
+++ task2statemet.py
@@ -0,0 +1,13 @@
+list_data=[1,2,3,4,5,6,7,8,9,10]
+
+if list_data[0]<5:
+    
+    small_number=[list_data[0],list_data[1],
+
+list_data[2],list_data[3]]
+
+if list_data[5]>5:
+    big_number=[list_data[5],list_data[6],list_data[7],list_data[8],list_data[9] ]  
+    print(small_number)
+    print(big_number)
+
