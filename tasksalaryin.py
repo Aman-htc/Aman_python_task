2d5e85d7bdb4983d4ac11fad2fcfb3beed3160d6
Aman kushwaha <amanhtc1112kushwaha@gmail.com>
2025-03-25 17:00:03 +0530

    input  four salary and maximum 2 higher print

---
+++ tasksalaryin.py
@@ -0,0 +1,55 @@
+list_data=[]
+
+salary=input('please enter your salary 1: ')
+if salary.isdigit():
+    salary=int(salary)
+    if salary >=10000 and salary<=100000:
+        list_data.append(salary)
+       
+        salary=input('please enter your salary 2: ')
+        if salary.isdigit():
+            salary=int(salary)
+            if salary >=10000 and salary<=100000:
+                
+                list_data.append(salary)
+               
+                salary=input('please enter your salary 3: ')
+                if salary.isdigit():
+                    
+                    salary=int(salary)
+                    if salary >=10000 and salary<=100000:
+                       list_data.append(salary)
+                       salary=input('please enter your salary 4: ')
+                       if salary.isdigit():
+                           salary=int(salary)
+                           if salary >=10000 and salary <=100000:
+                               list_data.append(salary)
+                               print('Top two higher salary!') 
+
+                               list_data.sort()
+                               print(list_data[2:])
+
+                               
+                           else:
+                               print('please enter you minimum 10000 and maximum 100000')    
+                       else:
+                           print('enter your number')    
+                        
+                    else:
+                        print('please enter you minimum 10000 and maximum 100000')   
+                else:
+                    print('enter your number!')
+    
+            else:
+                
+                print('please enter you minimum 10000 and maximum 100000')   
+
+        else:
+           print('enter your number!')
+    
+    else:
+        print('please enter you minimum 10000 and maximum 100000')   
+
+else:
+    print('enter your number!')
+    
